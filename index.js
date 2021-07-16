751bdf4b83e3bcc995fe2218463d64c5342bc40c
Arslan Ahmad <arslanahmadiub@gmail.com>
2021-07-16 14:44:19 +0500

    Testing Produciton

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const expressStaticGzip = require("express-static-gzip");
+
+// app.use(express.json({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, "build")));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+// app.listen(3000);
+
+const buildPath = path.join(__dirname, "build");
+app.use(
+  "/",
+  expressStaticGzip(buildPath, {
+    enableBrotli: true,
+    orderPreference: ["br", "gz"],
+  })
+);
+
+// Fallback to index.html when something that doesn't exist is requested
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
